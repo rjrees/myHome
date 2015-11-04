6393db33eb73cc6fc64a7cf82edade0f0fdf3b96
rjrees <rjrees@hotmail.com>
2015-11-04 18:57:48 +0000

    Fixed SQL Creation script with Foreign Keys

--- db/myHome.sql
+++ db/myHome.sql
@@ -1,36 +1,40 @@
-
-CREATE TABLE RecipeIngredients
+USE myHome;
+CREATE TABLE IF NOT EXISTS  Recipe
 (
-ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+uid varchar(10),
+name varchar(500),
+description varchar(500),
+created_on datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+updated datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Recipe';
+
+CREATE TABLE IF NOT EXISTS RecipeIngredients
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 UID varchar(10),
 Description varchar(255),
 Receipe int,
-Created date,
-updated date
-);
+created_on datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+updated datetime(6),
+FOREIGN KEY (id) REFERENCES Recipe(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Recipe';
 
-CREATE TABLE RecipeSteps
+CREATE TABLE IF NOT EXISTS  RecipeSteps
 (
 ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 UID varchar(10),
 StepPosition int,
 Step varchar(500),
 Recipe int,
-Created date,
-updated date
-);
+created_on datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+updated date,
+PRIMARY KEY (`id`),
+FOREIGN KEY (id) REFERENCES Recipe(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Recipe';
 
-CREATE TABLE Recipe
-(
-ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-UID varchar(10),
-Name varchar(500),
-Description varchar(500),
-Created date,
-updated date
-);
-
-CREATE TABLE BankStatement
+CREATE TABLE IF NOT EXISTS  BankStatement
 (
 ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 UID varchar(10),
@@ -44,15 +48,17 @@ Credit int,
 Debit int,
 Ballance int,
 ACL int,
-);
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Recipe';
 
-CREATE TABLE messages
+CREATE TABLE IF NOT EXISTS  messages
 (
 ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 UID varchar(10),
-Created date,
-Validfrom date,
+Created date, `created_on` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+ValidFrom date,
 ValidExpiration date,
 Description varchar(100),
 Message varchar(500),
-);
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Recipe';
