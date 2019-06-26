398dd425e9c6d352aae8ab6274e9564f87d6de65
Richard Rees <Richard.Rees@bjss.com>
2019-06-26 16:08:54 +0100

    Trying to get the mariadb service to connect

--- start-dev.sh
+++ start-dev.sh
@@ -5,7 +5,8 @@ function dev-local()
 {
     kubectl config get-contexts
     kubectl config use-context docker-for-desktop
-    kubectl create -f  k8/templates/db/db.yaml
+    kubectl create -f k8/templates/db/db.yaml
+    kubectl create -f k8/templates/java/java.yaml
     #docker run --name some-mariadb -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mariadb:tag
 }
 
