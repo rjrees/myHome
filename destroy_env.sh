c4a286259f6dbc3306d76d16740ccf2fbc6caee8
Richard Rees <rjreees@hotmail.com>
2020-01-07 19:22:02 +0000

    Some work on the train

--- destroy_env.sh
+++ destroy_env.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 kubectl config get-contexts
-kubectl config use-context docker-for-desktop
+kubectl config use-context docker-for-desktopx
 
 kubectl delete persistentvolumeclaims mariadb-pv mariadbslave-pv
 kubectl delete persistentvolumes mariadb-pv mariadbslave-pv
 kubectl delete services mariadb
-kubectl delete pod java mariadb
