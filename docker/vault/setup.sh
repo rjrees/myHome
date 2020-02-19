ab43eb44967a5d6853df0c269ab57452acff3bba
Richard Rees <Richard.Rees@bjss.com>
2020-02-19 11:57:51 +0000

    Added some vault steps, yet to create the policy for the application

--- docker/vault/setup.sh
+++ docker/vault/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-vault login 
+vault login
+vault secrets enable pki
+vault secrets enable transit
+vault secrets enable database
+vault secrets enable ssh
 vault policy write admin-policy admin-policy.hcl
 vault policy write provisioner-policy provisioner-policy.hcl
