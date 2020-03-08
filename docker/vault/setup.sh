036bacb4b85f852c54022eff1d322b8bf53c3c2c
Richard Rees <Richard.Rees@bjss.com>
2020-03-08 11:42:43 +0000

    Adding some policies

--- docker/vault/setup.sh
+++ docker/vault/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 vault login
+vault -autocomplete-install
 vault secrets enable pki
 vault secrets enable transit
 vault secrets enable database
 vault secrets enable ssh
 vault policy write admin-policy admin-policy.hcl
 vault policy write provisioner-policy provisioner-policy.hcl
+vault policy write myHome myHome-acl.hcl
